170a52bceb21e7caa864a8cddb79246ec87770d6
spacewolf <wolfmaniii@gmail.com>
2026-06-28 14:13:03 +0200

    chore(vitest): exclude e2e/** from Vitest test collection

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/test-setup.js'],
+    exclude: ['node_modules', 'e2e/**'],
     coverage: {
       provider: 'v8',
       include: ['src/**/*.{js,jsx}'],
